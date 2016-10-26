a42394e94df2a25240b5798beb370d457ed742a5
Daniel Gaunled <gaunled@gmail.com>
2016-10-26 10:36:12 +0700

    First Commit

---
+++ app/src/main/java/com/example/user/petsource/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.example.user.petsource;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public void gotoSignin(View view) {
+        Intent intent = new Intent(this, SigninActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void gotoRegister(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_login);
+    }
+}
