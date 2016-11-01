2ee77873df0bdb933d8441feee2331a00807d696
Daniel Gaunled <gaunled@gmail.com>
2016-11-01 18:15:18 +0700

    Tabbed

---
+++ app/src/main/java/com/example/user/petsource/homeFragments/HomeFragment.java
@@ -0,0 +1,28 @@
+package com.example.user.petsource.homeFragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.user.petsource.R;
+
+public class HomeFragment extends Fragment{
+    public HomeFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
