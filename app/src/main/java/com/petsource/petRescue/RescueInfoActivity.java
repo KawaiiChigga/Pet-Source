765b774be6cda2d527814260ba5a0e2cfa6ba733
Warwolfer <warwolfer@yahoo.com>
2016-11-21 17:10:34 +0700

    Buang semua

---
+++ app/src/main/java/com/petsource/petRescue/RescueInfoActivity.java
@@ -0,0 +1,110 @@
+package com.petsource.petRescue;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.petsource.MapsActivity;
+import com.petsource.R;
+import com.petsource.SplashActivity;
+import com.petsource.model.Pet;
+import com.petsource.network.API;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class RescueInfoActivity extends AppCompatActivity {
+
+    private Button btnCare;
+    private TextView lblCareTitle;
+    private Spinner spinner;
+    List<String> mypet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rescue_info);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/FRADMCN.TTF");
+
+
+        mypet = new ArrayList<String>();
+
+        Call<List<Pet>> getpet = API.Factory.getInstance().getPets(SplashActivity.mFirebaseAuth.getCurrentUser().getUid());
+        getpet.enqueue(new Callback<List<Pet>>() {
+            @Override
+            public void onResponse(Call<List<Pet>> call, Response<List<Pet>> response) {
+                for (Pet p : response.body() ) {
+                    mypet.add(p.getName());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Pet>> call, Throwable t) {
+
+            }
+        });
+
+
+        btnCare = (Button) findViewById(R.id.btnCare);
+        btnCare.setTypeface(typeface);
+
+        lblCareTitle = (TextView) findViewById(R.id.lblCareTitle);
+        lblCareTitle.setTypeface(typeface);
+
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.array, android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<String> petAdapter = new ArrayAdapter<String>(getBaseContext(), R.layout.spinner_layout, mypet);
+
+
+        // Specify the layout to use when the list of choices appears
+        petAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        spinner = (Spinner) findViewById(R.id.spinner2);
+        spinner.setAdapter(petAdapter);
+        spinner.setPrompt("Select pets");
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("PO123", position + " hai ");
+                spinner.setOnItemSelectedListener(this);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // Apply the adapter to the spinner
+
+    }
+
+    public void gotoMaps(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+}
+
+
