4d33b7c511157752457a37759de0bd14b425dddc
Daniel Gaunled <gaunled@gmail.com>
2016-12-02 01:26:02 +0700

    ujicba push notification

---
+++ app/src/main/java/com/petsource/FirebaseInstanceIDService.java
@@ -0,0 +1,27 @@
+package com.petsource;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Daniel on 12/2/2016.
+ */
+
+public class FirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+
+    }
+}
